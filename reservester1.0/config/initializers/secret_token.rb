4dfbb01054452e1b8b6102b6ea0a45816e4c3728
Peter Brown <peterbrownpa@gmail.com>
2014-05-21 15:34:49 -0700

    postgres working pg dumpall then psql then createdb then psql -h localhost

---
+++ reservester1.0/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reservester10::Application.config.secret_key_base = '7b1c5666b183e491fc7424fd3ce49c95347f3c19506a953a94ca4040c31a7589b7bf8dde06f4287b24e24e671100d64b2e489bc8c2e3bbb849c0f9022eaa7886'
